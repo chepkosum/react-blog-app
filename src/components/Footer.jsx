55be5c18682978952a513b75e906aa1a25661e4d
EISENHOWER <70235379+chepkosum@users.noreply.github.com>
2023-12-22 00:44:17 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
       <ul className='footer__categories'>
         <li><Link to="/posts/categories/agriculture">Agriculture</Link></li>
         <li><Link to="/posts/categories/agriculture">Education</Link></li>
-        <li><Link to="/posts/categories/agriculture">Agriculture</Link></li>
-        <li><Link to="/posts/categories/agriculture">Agriculture</Link></li>
-        <li><Link to="/posts/categories/agriculture">Agriculture</Link></li>
+        <li><Link to="/posts/categories/agriculture">Tourism</Link></li>
+        <li><Link to="/posts/categories/agriculture">Artificial Intelligence</Link></li>
+        <li><Link to="/posts/categories/agriculture">Transport</Link></li>
         <li><Link to="/posts/categories/agriculture">Arts</Link></li>
         <li><Link to="/posts/categories/agriculture">Technology</Link></li>
       </ul>
